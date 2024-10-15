8c5a6a015a81926ada0de2a29bd5ec1c2dc69fcd
XcinFinity <lyndon.bin.914@gmail.com>
2024-10-15 09:03:03 +0800

    ----

--- FinTech/1_hello.py
+++ FinTech/1_hello.py
@@ -3,3 +3,14 @@ print('hello,world')
 name = input ('plaese enter your name: ')
 print(f'helllo,{name}')
 
+
+a = 1
+b = 2
+c = a + b
+print(c)
+
+print(a)
+
+name = "Alice" 
+age = 30 
+print(f"Name: {name}, Age: {age}")
